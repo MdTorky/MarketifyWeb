27b705d1fa1a534a9ded3aa7425f0a95a0c5f569
cargo <mohamed2003torky@gmail.com>
2024-02-27 03:21:55 +0200

    Added Manage Accounts and Products Pages

--- src/pages/Admin/ManageAccounts.js
+++ src/pages/Admin/ManageAccounts.js
@@ -2,6 +2,28 @@ import './Admin.css'
 import { Icon } from '@iconify-icon/react';
 
 const ManageAccounts = () => {
+
+
+
+    const TableData = (status, fine) => {
+        return (
+            <tr className="TableData">
+                <td><button className="DeleteButton"><Icon icon="mdi:delete-circle" /></button>1</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td className={`${status ? "StatusGreen" : 'StatusRed'}`}>
+                    {status ? "Active" : "Inactive"}
+                    <button className="StatusButton">{!status ? "Activate" : "Deactivate"}</button>
+                </td>
+
+                <td>{fine ? fine + " RM" : <button className='TableButton'>Add</button>}</td>
+            </tr>
+        )
+    }
+
     return (
         <div className="Admin">
             <h2>Manage  Accounts</h2>
@@ -20,9 +42,24 @@ const ManageAccounts = () => {
                         <p>Total Accounts</p>
                     </div>
                 </div>
+            </div>
 
+            <div className="AdminBottomRow">
+                <table>
+                    <tr className='TableHeading'>
+                        <th>Id</th>
+                        <th>Username</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone No</th>
+                        <th>Address</th>
+                        <th>Status</th>
+                        <th>Fine</th>
+                    </tr>
+                    {TableData(true, 12)}
+                    {TableData(false, null)}
 
-
+                </table>
             </div>
         </div>
     );
--- src/pages/Admin/ManageProducts.js
+++ src/pages/Admin/ManageProducts.js
@@ -2,9 +2,27 @@ import './Admin.css'
 import { Icon } from '@iconify-icon/react';
 
 const ManageProducts = () => {
+    const TableData = () => {
+        return (
+            <tr className="TableData">
+                <td><button className="DeleteButton"><Icon icon="mdi:delete-circle" /></button>1</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                <td>Data</td>
+                {/* <td className={`${status ? "StatusGreen" : 'StatusRed'}`}>
+                    {status ? "Active" : "Inactive"}
+                    <button className="StatusButton">{!status ? "Activate" : "Deactivate"}</button>
+                </td> */}
+
+                <td><button className='TableButton'>Delete</button></td>
+            </tr>
+        )
+    }
+
     return (
         <div className="Admin">
-            <h2>Manage  Accounts</h2>
+            <h2>Manage  Products</h2>
             <div className="AdminTopRow">
                 <div className="SearchBox">
                     <input className="searchInput" type="text" name="" placeholder="Search something" />
@@ -14,15 +32,30 @@ const ManageProducts = () => {
                 </div>
 
                 <div className="InfoCard">
-                    <Icon icon="mdi:accounts-group" className='InfoCardIcon' />
+                    <Icon icon="fluent-mdl2:product-variant" className='InfoCardIcon' />
                     <div className="InfoCardText">
                         <h3>2</h3>
-                        <p>Total Accounts</p>
+                        <p>Total Products</p>
                     </div>
                 </div>
 
+            </div>
 
+            <div className="AdminBottomRow">
+                <table>
+                    <tr className='TableHeading'>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Seller</th>
+                        <th>Phone No</th>
+                        <th>Action</th>
+                        {/* <th>Fine</th> */}
+                    </tr>
+                    {TableData()}
+                    {TableData()}
 
+                </table>
             </div>
         </div>
     );
